6b4abcb66581b7ad09c1d6733c2c90ed188d82b3
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-05-09 01:44:51 +0800

    Added pseudocodes

---
+++ simple_calculator.py
@@ -0,0 +1,14 @@
+# Diong, Shan Marc C.
+# BSCPE 1-2
+
+# Ask the user what of the four operations will be used
+
+# Ask the user for two numbers
+
+# Display results
+
+# Ask the user whether to try again or not, if not display grateful message
+
+# Input Validations
+
+# Display errors
