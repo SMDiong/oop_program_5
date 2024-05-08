3d0f7ec0e998d481157849a0d862fc7424a94087
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-05-09 03:53:44 +0800

    Added the code for greetings and the math operations to be used

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,9 +1,17 @@
 # Diong, Shan Marc C.
 # BSCPE 1-2
 
-# Ask the user what of the four operations will be used
+# Greet the user
+user_name = input("Please enter your name: ")
+print(f"\nHello {user_name}! Welcome to my Simple App Calculator!")
 
-# Ask the user for two numbers
+# Ask what of the four operations will be used
+while True:
+    print("\nChoose one of the four math operations by their corresponding initials.")
+    print("(A)ddition, (S)ubtraction, (M)ultiplication, (D)ivision.")
+    math_operation = input("\nPlease enter your choice (A/S/M/D): ")
+
+# Ask the user for how many numbers to solve
 
 # Display results
 
